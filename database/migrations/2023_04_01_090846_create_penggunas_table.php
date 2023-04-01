bae506371786b296e7505786d2dea608d79fd29e
DikkyRahmadS <dikkyrahmads@gmail.com>
2023-04-01 18:08:56 +0700

    migration user

---
+++ database/migrations/2023_04_01_090846_create_penggunas_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('penggunas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->interger('pin', 6);
+            $table->string('image');
+            $table->boolean('role')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('penggunas');
+    }
+};
