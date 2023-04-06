1f3ccd7c1d238b5d22ed22725a337a1141934e97
Ilham-Maulidin <ilhammaulidin20232023@gmail.com>
2023-04-06 10:52:07 +0700

    update tabel suppliers

--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -8,7 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Supplier extends Model
 {
     use HasFactory;
-    protected $table =  'supplier';
     protected $fillable = [
         'nama', 'alamat', 'no_hp'
     ];
--- database/migrations/2023_04_04_122039_create_suppliers_table.php
+++ database/migrations/2023_04_04_122039_create_suppliers_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('supplier', function (Blueprint $table) {
+        Schema::create('suppliers', function (Blueprint $table) {
             $table->id();
             $table->string('nama');
             $table->text('alamat');
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('supplier');
+        Schema::dropIfExists('suppliers');
     }
 };
