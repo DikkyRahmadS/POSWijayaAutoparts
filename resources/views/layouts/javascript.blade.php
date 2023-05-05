5b5d962a9b1ab79a9657c1467e57d8621d3c8a18
fardivn <farahdivanabila5@gmail.com>
2023-05-05 07:40:56 +0700

    membetulkan laporan

--- resources/views/layouts/javascript.blade.php
+++ resources/views/layouts/javascript.blade.php
@@ -20,5 +20,6 @@
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
 <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
 <script src="{{ asset('js/validator.min.js') }}"></script>
 @stack('scripts')
